cb604ea75924e0f1b00a70a468d8cb936a471c15
pavanthi sudasinghe <pavanthisppm@gmail.com>
2024-02-17 23:33:01 +0530

    update files
    2024-02-17

--- Cloudline-Month.R
+++ Cloudline-Month.R
@@ -9,21 +9,29 @@ data
 class(data$Cloudline)
 
 data$Cloudline = as.numeric(as.character(data$Cloudline))
+data$rain = as.numeric(as.character(data$rain))
 
 library(ggplot2)
 
 png("Cloudline-Month.png", height = 500, width = 700)
 
-ggplot(data, aes(x = Sampling_Session, y = Cloudline, size = Freq)) +
+ggplot(data, aes(x = Sampling_Session, y = Cloudline, size = Freq, group = 1)) +
   geom_point()+ 
   geom_text(aes(label = paste(Cloudline,"(",Freq,")")), size = 4, vjust = 2.2) +
   scale_size(name = "Size", range = c(5, 12)) +
   theme(legend.position = "none")+
   labs(x = "Month", y = "Elevation of the cloudline", title = " ") +
+  geom_line( aes(y = rain/0.02),   color = "blue", size=0.1) +
+  geom_text(aes(y = rain/0.02, label= rain),   hjust = -0.5, vjust = 1,  size= 3, color= "blue") +
   scale_x_discrete(breaks = 1:6, labels = c("Nov", "Dec", "Jan", "Feb", "Mar", "Apr"))+
-  scale_y_continuous(name = "Cloudline Elevation", limits = c(0, 2000), breaks = seq(0, 2000, by = 500))
+  scale_y_continuous(name = "Cloudline Elevation", limits = c(0, 2000), breaks = seq(0, 2000, by = 500), sec.axis = sec_axis( trans=~.*0.02, name="Rainfall"))
+  
 
 dev.off()
+
+class(data$rain)
+data$rain
+
 
  
 
--- chisquare.R
+++ chisquare.R
@@ -60,12 +60,22 @@ Low_land
 Mid_land
 High_land
 
+################################### Chi Square 1###############################################
 
 data1 = ToPawanthi_Master_data_set_new_2_
 data1
 
 # Low_land
-col_no = which(colnames(data1) %in% Low_land)
+col_no_low = which(colnames(data1) %in% Low_land)
+col_no_mid = which(colnames(data1) %in% Mid_land)
+col_no_high = which(colnames(data1) %in% High_land)
+
+
+# a = c(1,1,2,3,4,4,5,5,6)
+# b = c(2,2,3,4,4,5)
+# duplicated(a,b)
+# 
+# unique(a[which(a %in% b)])
 
 a = c()
 for (i in col_no){
@@ -161,3 +171,102 @@ print(chi_square_result)
 # fisher test
 fisher.test(data_matrix)
 
+
+################################## Chi Sqaure 2###################################################
+
+data1 = ToPawanthi_Master_data_set_new_4_
+
+
+# create the variable transect
+
+# 480-700
+data1$transect = NA
+
+for (i in 1:length(data1$Sampling_Session)){
+  if (480<=data1$`Transect_&_Elevation`[i] & data1$`Transect_&_Elevation`[i]<=700){
+    data1$transect[i] = "Low"
+  }
+}
+
+
+# 780-1000
+
+for (i in 1:length(data1$Sampling_Session)){
+  if (780<=data1$`Transect_&_Elevation`[i] & data1$`Transect_&_Elevation`[i]<=1000){
+    data1$transect[i] = "Mid"
+  }
+}
+
+# 1100 - 1420
+
+for (i in 1:length(data1$Sampling_Session)){
+  if (1100<=data1$`Transect_&_Elevation`[i] & data1$`Transect_&_Elevation`[i]<=1420){
+    data1$transect[i] = "High"
+  }
+}
+
+data1$transect[34]
+data1$`Transect_&_Elevation`[34]
+
+
+
+
+
+which(!(is.na(data1$`Emerald Dove`)))
+data2$transect[which(!(is.na(data2$`Emerald Dove`)))]
+d1_col_names = colnames(data1)[16:112]
+data2$transect
+data2 = data1[16:113]
+colnames(data2)
+
+
+
+for (i in 1:97){
+    cat(d1_col_names[i] ,  table(data2$transect[which(!(is.na(data2[i])))]), "\n")
+}
+ 
+
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+
+a = c()
+for (i in 1:97){
+  a[i] = getmode(data2$transect[!is.na(data2[[i]])])
+}
+a
+
+ 
+LG100 = 0
+LL100 = 0
+MG100 = 0
+ML100 = 0
+HG100 = 0
+HL100 = 0
+
+data1$Diff = abs(data1$`Transect_&_Elevation`-data1$Cloudline)
+
+for (i in 1:length(a)){
+  if (data1$Diff[1]>=100 & data1$transect == "Low"){
+    LG100 = LG100+1
+  }else if (data1$Diff[i]<100 & data1$transect == "Low"){
+    LL100 = LL100+1
+  }else if(data1$Diff[i]>=100 & data1$transect == "Mid"){
+    MG100 = MG100+1
+  }else if (data1$Diff[i]<100 & data1$transect == "Mid"){
+    ML100 = ML100+1
+  }else if (data1$Diff[i]>=100 & data1$transect == "High"){
+    HG100 = HG100+1
+  }else if (data1$Diff[i]<100 & data1$transect == "High"){
+    HL100 = HL100+1
+  }
+}
+
+# for (i in 1:97){
+#  
+#    print(table(data2$transect[which(!(is.na(data2[i])))]) )
+# }
+
+
+ 
